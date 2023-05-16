137ed9e88f877bee54852f894dbd44cf9b5892ec
Sena <36813961+helenaferdy@users.noreply.github.com>
2023-05-16 10:06:42 +0700

    final

---
+++ README.md
@@ -0,0 +1,8 @@
+# Chatbot Telegram Bot
+
+## This is a Telegram bot that acts as a chatbot, using a pre-trained feed forward neural network model to generate responses based on user input.
+## The model is trained with the pre set data that can be found in intents.json file
+
+> try it yourself here : ![helena_ffnn_bot](static/t.me/helena_ffnn_bot)
+
+![capture](static/FFNN.png)
--- chat.py
+++ chat.py
@@ -5,13 +5,17 @@ from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 from train import all_words, tags, intents
 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 FILE = "model/FFN.pth"
 model = torch.load(FILE)
 
-def chatbot():
+def chatbot(message):
     model.eval()
     while True:
-        sentence = input('You : ')
+        # sentence = input('You : ')
+        sentence = message
+        print(f"Human : {sentence}")
         if sentence == "quit":
             break
 
@@ -39,3 +43,27 @@ def chatbot():
             return response
 
 # chatbot()
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! Welcome.")
+                         
+
+def handle_message(update, context):
+    message_text = update.message.text
+    response = chatbot(message_text)
+    # context.bot.send_message(chat_id=update.effective_chat.id, text=f"You said: {message_text}")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"{response}")
+
+# Set up the bot
+updater = Updater(token='6035117137:AAFBsUhD38QM9Jb_O4Run-KEKcYENpWtx2I', use_context=True)
+
+# Get the dispatcher to register handlers
+dispatcher = updater.dispatcher
+
+# Add handlers for commands and messages
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+
+# Start the bot
+updater.start_polling()
